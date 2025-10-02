99e9bb113a9bcaacd6577fcca504474b08b65d4c
Nikolay1727 <cnm17@yandex.ru>
2025-10-02 14:56:35 +0300

    Fixed rackets page

--- src/app/rackets/page.tsx
+++ src/app/rackets/page.tsx
@@ -1,8 +1,23 @@
-import { RacketCardsBox } from "@/components";
-import { rackets } from "../../../public";
+import { RacketCardsBox, SuspenseWrapper } from "@/components";
+import { getRackets } from "@/services";
+import { notFound } from "next/navigation";
 
-const Rackets = () => {
-  return <RacketCardsBox rackets={rackets} />;
+const Rackets = async () => {
+  const { isError, data } = await getRackets({ limit: 20 });
+
+  if (isError) {
+    return "error";
+  }
+
+  if (!data) {
+    return notFound();
+  }
+
+  return (
+    <SuspenseWrapper>
+      <RacketCardsBox rackets={data} />
+    </SuspenseWrapper>
+  );
 };
 
 export default Rackets;
