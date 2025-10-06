9ecfa5474fc3ea6cfdfecd53b09d5247df467769
Nikolay1727 <cnm17@yandex.ru>
2025-10-06 22:05:04 +0300

    Added error handlers

---
+++ src/app/error.tsx
@@ -0,0 +1,5 @@
+"use client";
+
+const Error = () => <div>some error</div>;
+
+export default Error;
---
+++ src/app/global-error.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+const GlobalError = () => (
+  <html>
+    <body>
+      <div>global error</div>
+    </body>
+  </html>
+);
+
+export default GlobalError;
